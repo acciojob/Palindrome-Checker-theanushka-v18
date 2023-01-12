fe84fb2b203128f025491e888d4bef3903bd428f
Anushka Verma <93106932+theanushka-v18@users.noreply.github.com>
2023-01-12 10:56:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,22 @@
 
 function palindrome(str){
 
+	let i = 0;
+	let j = str.length()-1;
+	let flag = false;
+	while(i <= j) {
+		if(str[i]!=str[j]) {
+			flag = true;
+			break;
+		}
+		i++;
+		j--;
+	}
+	if(flag != true) {
+		return true;
+	} else {
+		return false;
+	}
+
 }
 module.exports = palindrome
