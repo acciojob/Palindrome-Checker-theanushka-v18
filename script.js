ebc801a06a92729cd38c224fd75b92aec8297998
Anushka Verma <93106932+theanushka-v18@users.noreply.github.com>
2023-01-17 19:45:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,10 @@
 
 function palindrome(str){
 
-	let s = str.reverse();
-	if(s===str) {
-		return true;
-	} else {
-		return false;
-	}
+var re = /[\W_]/g;
+var lowRegStr = str.toLowerCase().replace(re, '');
+var reverseStr = lowRegStr.split('').reverse().join(''); 
+return reverseStr === lowRegStr;
 
 }
 module.exports = palindrome
